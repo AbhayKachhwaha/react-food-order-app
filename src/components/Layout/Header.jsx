09d7c82fcbcc840445c2012cd4c34139f44b15fc
AbhayKachhwaha <abhay.kachhwah09@gmail.com>
2021-06-15 18:04:01 +0530

    Initial commit

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <React.Fragment>
+            <header>
+                <div>React Meals</div>
+                <button>Add To Cart</button>
+            </header>
+            <div></div>
+        </React.Fragment>
+    )
+}
+
+export default Header
