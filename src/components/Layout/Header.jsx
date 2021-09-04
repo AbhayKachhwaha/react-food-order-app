06bd77b76b73ceba4721bf1b5b98733ee44419a4
AbhayKachhwaha <abhay.kachhwah09@gmail.com>
2021-09-04 19:45:30 +0530

    Added components

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React from "react";
+import styles from "./Header.module.css";
+import mealsImg from "../../assets/meals.jpg";
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = () => {
-    return (
-        <React.Fragment>
-            <header>
-                <div>React Meals</div>
-                <button>Add To Cart</button>
-            </header>
-            <div></div>
-        </React.Fragment>
-    )
-}
+  return (
+    <React.Fragment>
+      <header className={styles.header}>
+        <div>React Meals</div>
+        <HeaderCartButton />
+      </header>
+      <div className={styles[`main-image`]}>
+        <img src={mealsImg} alt="Meals" />
+      </div>
+    </React.Fragment>
+  );
+};
 
-export default Header
+export default Header;
