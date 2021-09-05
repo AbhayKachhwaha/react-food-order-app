9aa8a11c7c90c894b8d6fe2230b999ec2a403a78
AbhayKachhwaha <abhay.kachhwah09@gmail.com>
2021-09-05 12:09:30 +0530

    Bug fix

--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { ReactDOM } from "react-dom";
+import React, { Fragment } from "react";
+import ReactDOM from "react-dom";
 import classes from "./Modal.module.css";
 
 const Backdrop = (props) => {
@@ -17,17 +17,17 @@ const ModalOverlay = (props) => {
 const portalElement = document.getElementById("overlays");
 
 const Modal = (props) => {
-  return (<div></div>
-    // <React.Fragment>
-    //   {ReactDOM.createPortal(
-    //     <Backdrop onClose={props.onClose} />,
-    //     portalElement
-    //   )}
-    //   {ReactDOM.createPortal(
-    //     <ModalOverlay>{props.children}</ModalOverlay>,
-    //     portalElement
-    //   )}
-    // </React.Fragment>
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop onClose={props.onClose} />,
+        portalElement
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </Fragment>
   );
 };
 
